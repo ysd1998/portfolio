ea1968f0eedb4c332cbbc5025c1c92f5b0a052a8
ysd1998 <sedung0123@gmail.com>
2024-04-07 16:38:20 +0900

    文字制限 追加

--- src/main/java/com/example/demo/controller/secondary/ManagerAdminController.java
+++ src/main/java/com/example/demo/controller/secondary/ManagerAdminController.java
@@ -74,7 +74,14 @@ public class ManagerAdminController {
 			List<Types> result = typeservice.serchData("%");
 			model.addAttribute("Types", result);
 			model.addAttribute("errorMsg","必須項目が空欄です。");
-			return "/manager/{id}";
+			return "/manager/bookinit";
+		} else if (isCorrectUserAuth.length() > 10 || isTitle.length() > 10 || bookData.getPublisher().length() > 10 ||
+				bookData.getAuther().length() > 10 || bookData.getEx().length() > 1000 || bookData.getOther().length() > 1000||
+				bookData.getPrice().length() > 10) {
+			List<Types> result = typeservice.serchData("%");
+			model.addAttribute("Types", result);
+			model.addAttribute("errorMsg","文字数オーバーです。");
+			return "/manager/bookinit";
 		} else {
 			return "redirect:/manager/confirm";
 			
--- src/main/java/com/example/demo/controller/secondary/ManagerInsertController.java
+++ src/main/java/com/example/demo/controller/secondary/ManagerInsertController.java
@@ -89,11 +89,12 @@ public class ManagerInsertController {
 			model.addAttribute("Types", result);
 			model.addAttribute("errorMsg","必須項目が空欄です。");
 			return "/manager/bookinit";
-		} else if (isCorrectUserAuth.length() >= 8 || isTitle.length() >= 10 || bookData.getPublisher().length() >= 10 ||
-				bookData.getAuther().length() >= 10) {
+		} else if (isCorrectUserAuth.length() > 10 || isTitle.length() > 10 || bookData.getPublisher().length() > 10 ||
+				bookData.getAuther().length() > 10 || bookData.getEx().length() > 1000 || bookData.getOther().length() > 1000||
+				bookData.getPrice().length() > 10) {
 			List<Types> result = typeservice.serchData("%");
 			model.addAttribute("Types", result);
-			model.addAttribute("errorMsg","必須項目が入力されていません。");
+			model.addAttribute("errorMsg","文字数オーバーです。");
 			return "/manager/bookinit";
 		} else {
 			return "redirect:/manager/confirm";

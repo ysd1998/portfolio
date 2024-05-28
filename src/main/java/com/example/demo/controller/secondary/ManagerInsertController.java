8c5c3f10cc248fe94eeacd5009dc0b5f095fe760
ysd1998 <sedung0123@gmail.com>
2024-05-28 15:38:45 +0900

    微修正

--- src/main/java/com/example/demo/controller/secondary/ManagerInsertController.java
+++ src/main/java/com/example/demo/controller/secondary/ManagerInsertController.java
@@ -85,6 +85,7 @@ public class ManagerInsertController {
 		Date date = new Date();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		String strDate = dateFormat.format(date);
+		//		String bookDate = dateFormat.format(bookData.getYear());
 		bookData.setInsertday(strDate);
 		bookData.setInsertid(user.getUsername());
 		bookData.setDeleteflag("0");
@@ -92,13 +93,14 @@ public class ManagerInsertController {
 		if (isCorrectUserAuth.equals("") || isTitle.equals("") || "".equals(bookData.getPublisher()) ||
 				"".equals(bookData.getYear()) || "0".equals(bookData.getTypeid()) || "".equals(bookData.getPrice())
 				|| "".equals(bookData.getEx())) {
+			// || bookDate.equals(null)
 			List<Types> result = typeservice.serchData("%");
 			model.addAttribute("Types", result);
 			model.addAttribute("errorMsg", "必須項目が空欄です。");
 			return "manager/bookinit";
 		} else if (isCorrectUserAuth.length() > 10 || isTitle.length() > 10 || bookData.getPublisher().length() > 10 ||
 				bookData.getAuther().length() > 10 || bookData.getEx().length() > 1000
-				|| bookData.getOther().length() > 1000 ||
+				|| bookData.getOther().length() > 1000 || bookData.getYear().length() != 8 ||
 				bookData.getPrice().length() > 10) {
 			List<Types> result = typeservice.serchData("%");
 			model.addAttribute("Types", result);

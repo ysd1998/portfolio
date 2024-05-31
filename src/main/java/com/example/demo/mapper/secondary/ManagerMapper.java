1fc1f74b008a55d5b092f9dd7f5a265ce0548e6b
ysd1998 <sedung0123@gmail.com>
2024-05-31 17:08:51 +0900

    mapper変更

--- src/main/java/com/example/demo/mapper/secondary/ManagerMapper.java
+++ src/main/java/com/example/demo/mapper/secondary/ManagerMapper.java
@@ -1,5 +1,25 @@
 package com.example.demo.mapper.secondary;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.entity.employees.Employees;
+
+@Mapper
 public interface ManagerMapper {
+	
+	Page<Employees> findAll(Pageable page);
+
+	@Select("select u from Employees u where u.loginid like #{loginid} or u.auther like #{auther} or u.password like #{password}")
+	List<Employees> findByLoginidLikeOrPasswordLike(String loginid, String password);
+
+	@Select("select u from Employees u where u.loginid like :loginid or u.name like :name or u.authority like :authority")
+	Page<Employees> findByLoginidLikeOrNameLikeOrAuthorityLike(@Param("loginid") String loginid,
+			@Param("name") String name, @Param("authority") String authority, Pageable page);
 
 }
